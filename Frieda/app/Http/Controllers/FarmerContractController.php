2561b08df8446b334289c4e2bb558084c9087d30
Frieda <forestbear0118tw@gmail.com>
2020-10-28 14:57:37 +0800

    123

---
+++ Frieda/app/Http/Controllers/FarmerContractController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\FarmerContract;
+use App\Models\FarmerIntroduction;
+use App\Models\FarmerMemberShips;
+
+class FarmerContractController extends Controller
+{
+    //
+}
